e6021cb1b84cfccb02d4fc4c019e4eb494b9829f
Anna Roos <anna.flytstrom@gmail.com>
2022-02-13 20:01:29 +0100

    sending http requests together with redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +7,23 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch('', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      const data = await response.json();
+    };
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
